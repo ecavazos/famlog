3969c94fe40618a6066078bd457df83fbcc3a044
ecavazos <ejcavazos@gmail.com>
2010-04-27 22:25:36 -0700

    testing messages controller

--- test/functional/message_controller_test.rb
+++ test/functional/message_controller_test.rb
@@ -4,17 +4,33 @@ class MessagesControllerTest < ActionController::TestCase
 
   setup do
     # stub rails_warden helper method
+    @user = Factory.build(:user)
     ApplicationHelper.class_eval do
       def current_user
-        Factory.build(:user)
+        @user
       end
     end
-
+    @controller.expects(:current_user).returns(@user)
     @controller.expects(:require_user)
   end
 
-  test "should get new" do
+  test "should :get new" do
     get :new
     assert_response :success
+    assert_template :new
+    assert_not_nil assigns(:message)
+  end
+
+  test "should :get edit" do
+    Message.expects(:find).with(2).returns(Factory.build(:message))
+    get :edit, { :id => 2 }
+    assert_response :success
+    assert_template :edit
+    assert_not_nil assigns(:message)
+  end
+
+  test "should :post create" do
+    post :create
+    assert_redirected_to root_path
   end
 end
