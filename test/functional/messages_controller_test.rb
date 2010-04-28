396980e2daa8940b961354e328eb8993b7c968e9
ecavazos <ejcavazos@gmail.com>
2010-04-28 07:07:37 -0700

    testing

---
+++ test/functional/messages_controller_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class MessagesControllerTest < ActionController::TestCase
+
+  setup do
+    # stub rails_warden helper method
+    @user = Factory.build(:user)
+    ApplicationHelper.class_eval do
+      def current_user
+        @user
+      end
+    end
+    @controller.expects(:current_user).returns(@user)
+    @controller.expects(:require_user)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_template :new
+    assert_not_nil assigns(:message)
+  end
+
+  test "should get edit" do
+    Message.expects(:find).with(2).returns(Factory.build(:message))
+    get :edit, { :id => 2 }
+    assert_response :success
+    assert_template :edit
+    assert_not_nil assigns(:message)
+  end
+
+  test "should redirect to root when post to create is successful" do
+    post :create
+    message = Factory.build(:message)
+    message.expects(:set_user).with(@user)
+    Message.stubs(:find).returns(message)
+    assert_redirected_to root_path
+  end
+
+  test "should render new when post to create fails" do
+    post :create
+    message = Factory.build(:message)
+    message.expects(:save).returns(false)
+    Message.stubs(:find).returns(message)
+    assert_response :redirect
+    assert_template :new
+  end
+end
