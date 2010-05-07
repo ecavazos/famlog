1b47d096c56a1e013c7dcb5dd1ef37247d42d1da
ecavazos <ejcavazos@gmail.com>
2010-05-07 16:43:13 -0700

    a hack for heroku

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,26 +1,26 @@
-require 'uri'
-
-
-File.open(File.join(Rails.root, 'config/database.mongo.yml'), 'r') do |f|
-  if ENV['MONGOHQ_URL']
-    mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
-    puts mongo_uri
-    @settings = {}
-    @settings['host'] = mongo_uri.host
-    @settings['port'] = mongo_uri.port.to_s
-    @settings['username'] = mongo_uri.user
-    @settings['password'] = mongo_uri.password
-    @settings['database'] = mongo_uri.path.to_s.sub('/', '')
-    puts @settings
-  else
-    @settings = YAML.load(f)[Rails.env]
-  end
-end
-
-connection = Mongo::Connection.new(@settings["host"])
-Mongoid.database = connection.db(@settings["database"])
-
-if @settings["username"]
-  Mongoid.database.authenticate(@settings["username"], @settings["password"])
-end
+#require 'uri'
+#
+#
+#File.open(File.join(Rails.root, 'config/database.mongo.yml'), 'r') do |f|
+#  if ENV['MONGOHQ_URL']
+#    mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+#    puts mongo_uri
+#    @settings = {}
+#    @settings['host'] = mongo_uri.host
+#    @settings['port'] = mongo_uri.port.to_s
+#    @settings['username'] = mongo_uri.user
+#    @settings['password'] = mongo_uri.password
+#    @settings['database'] = mongo_uri.path.to_s.sub('/', '')
+#    puts @settings
+#  else
+#    @settings = YAML.load(f)[Rails.env]
+#  end
+#end
+#
+#connection = Mongo::Connection.new(@settings["host"])
+#Mongoid.database = connection.db(@settings["database"])
+#
+#if @settings["username"]
+#  Mongoid.database.authenticate(@settings["username"], @settings["password"])
+#end
 
