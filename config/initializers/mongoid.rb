d86442629760195caf3110b1f1f18edeb7fb3538
ecavazos <ejcavazos@gmail.com>
2010-05-07 06:27:41 -0700

    a hack for heroku

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,5 +1,18 @@
+require 'uri'
+
+
 File.open(File.join(Rails.root, 'config/database.mongo.yml'), 'r') do |f|
-  @settings = YAML.load(f)[Rails.env]
+  if ENV['MONGOHQ_URL']
+    puts'ass'
+    mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+    @settings['host'] = mongo_uri.host
+    @settings['port'] = mongo_uri.port.to_s
+    @settings['username'] = mongo_uri.user
+    @settings['password'] = mongo_uri.password
+    @settings['database'] = mongo_uri.path.gsub('/', '')
+  else
+    @settings = YAML.load(f)[Rails.env]
+  end
 end
 
 connection = Mongo::Connection.new(@settings["host"])
