1919ae49a273f78ef7172b188eda3291c9201186
ecavazos <ejcavazos@gmail.com>
2010-05-08 07:24:17 -0700

    fixing redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
   helper :all # include all helpers, all the time
-  
+
   before_filter :authenticate_user!
 end
