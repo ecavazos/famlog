ce34dc8515be36849ba068ef7a734d1c66beb4a7
ecavazos <ejcavazos@gmail.com>
2010-05-08 07:31:50 -0700

    fixing redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < Devise::SessionsController
-  prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
-
   def new
     clean_up_passwords(build_resource)
     render :layout => 'modal'
