4ce5b5843c8381f2d336342df599109531f62f70
ecavazos <ejcavazos@gmail.com>
2010-05-08 22:47:16 -0700

    cleaning up messages_controller and putting logic on to message entity

--- app/models/message.rb
+++ app/models/message.rb
@@ -32,4 +32,57 @@ class Message
   def end_date?
     is_event? && end_date && end_date > start_date
   end
+
+  class << self
+    def by_tab(tab)
+      case tab
+      when 'today' then
+        where(event_is_today_js)
+          .order_by([:start_date, :desc]).limit(20)
+      when 'forecast' then
+        where(event_is_this_week_js)
+          .order_by([:start_date, :asc]).limit(20)
+      when 'history'
+        where(:created_at.lt => Date.today.to_time.getutc)
+          .order_by([:created_at, :desc]).limit(20)
+      else
+        all.order_by([[:created_at, :desc]]).limit(20)
+      end
+    end
+
+    def event_is_today_js
+      <<JS
+        function () {
+          var now = new Date(),
+              day = now.getDate(),
+              mon = now.getMonth(),
+              yr  = now.getFullYear();
+
+          return this.is_event
+          && this.start_date >= new Date(yr, mon, day)
+          && this.start_date < new Date(yr, mon, day + 1)
+          || (this.start_date <= new Date(yr, mon, day)
+          && this.end_date >= new Date(yr, mon, day + 1));
+        }
+JS
+    end
+
+    def event_is_this_week_js
+      <<JS
+        function () {
+          var now = new Date(),
+              day = now.getDate(),
+              mon = now.getMonth(),
+              yr  = now.getFullYear();
+
+          return this.is_event
+          && this.start_date >= new Date(yr, mon, day)
+          && this.start_date <  new Date(yr, mon, day + 5)
+          || (this.end_date >= new Date(yr, mon, day)
+          && this.end_date < new Date(yr, mon, day + 5));
+        }
+JS
+    end
+
+  end
 end
