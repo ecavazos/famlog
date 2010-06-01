9f768a9c38fbc7fdd5a863ce72bff079e23a9df5
ecavazos <ejcavazos@gmail.com>
2010-06-01 07:24:56 -0700

    excluding index action on before filter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ require 'lib/famlog/controllers/messages'
 class MessagesController < ApplicationController
   include Famlog::Controllers::Messages
 
-  before_filter :find_message_and_verify_ownership, :except => [:new, :create]
+  before_filter :find_message_and_verify_ownership, :except => [:index, :new, :create]
 
   def index
     @messages = Message.by_tab params[:tab]
