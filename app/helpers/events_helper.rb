dde95268a6e72a924da98c369457c43f1dfcb439
ecavazos <ejcavazos@gmail.com>
2010-06-30 22:32:56 -0700

    testing for heroku bug

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,10 @@
 module EventsHelper
-  def times
+  def time_opts
     t = []
     format = lambda { |x| x.strftime('%l:%M %P').strip }
     24.times do |i|
-      t << format.call(Time.new(1999, 9, 9, 3, 00))
-#      t << format.call(Time.new(1999, 9, 9, i, 30))
+      t << format.call(Time.new(1999, 9, 9, i, 00))
+      t << format.call(Time.new(1999, 9, 9, i, 30))
     end
     t
   end
--- test/unit/helpers/events_helper_test.rb
+++ test/unit/helpers/events_helper_test.rb
@@ -1,24 +1,24 @@
 require 'test_helper'
 
 class EventsHelperTest < ActionView::TestCase
-  context 'times' do
+  context 'time_opts' do
     should 'have "12:00 am" as the first item in list' do
-      assert_equal '12:00 am', times.first
+      assert_equal '12:00 am', time_opts.first
     end
 
     should 'have 48 items' do
-      assert_equal times.length, 48
+      assert_equal time_opts.length, 48
     end
 
     should 'have "11:30 pm" as the last item in list' do
-      assert_equal '11:30 pm', times.last
+      assert_equal '11:30 pm', time_opts.last
     end
 
     should 'incriment by a half hour' do
-      assert_equal '12:00 am', times[0]
-      assert_equal '12:30 am', times[1]
-      assert_equal '1:00 am',  times[2]
-      assert_equal '1:30 am',  times[3]
+      assert_equal '12:00 am', time_opts[0]
+      assert_equal '12:30 am', time_opts[1]
+      assert_equal '1:00 am',  time_opts[2]
+      assert_equal '1:30 am',  time_opts[3]
     end
   end
 end
