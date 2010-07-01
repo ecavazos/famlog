72dc18172908e9e7a27758426e43857107142017
ecavazos <ejcavazos@gmail.com>
2010-06-30 22:09:04 -0700

    testing for heroku bug

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,8 +3,9 @@ module EventsHelper
     t = []
 #    format = lambda { |x| x.strftime('%l:%M %P').strip }
     24.times do |i|
-      t << format(Time.new(1999, 9, 9, i, 00))
-      t << format(Time.new(1999, 9, 9, i, 30))
+      t << "blah"
+#      t << format(Time.new(1999, 9, 9, i, 00))
+#      t << format(Time.new(1999, 9, 9, i, 30))
     end
     t
   end
